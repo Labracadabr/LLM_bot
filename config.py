56528d63b161dfd68c1b3d9e07dfc157d830300f
Dmitrii <dminokin@gmail.com>
2024-05-25 10:50:35 +0300

    лишние креды

--- config.py
+++ config.py
@@ -7,8 +7,6 @@ from settings import prod
 class Config:
     BOT_TOKEN: str = None           # телеграм бот
     GROQ_API_KEY: str = None
-    WEBHOOK_URL: str = None
-    SOME_MORE_TOKEN: str = None     # токен от еще чего-нибудь
 
     host: str = None                # хост
     dbname: str = None              # имя базы данных
@@ -23,8 +21,6 @@ env.read_env()
 config = Config(
     BOT_TOKEN=env('BOT_TOKEN_PROD') if prod else env('BOT_TOKEN_TEST'),
     GROQ_API_KEY=env('GROQ_API_KEY'),
-    WEBHOOK_URL=env('WEBHOOK_URL'),
-    # SOME_MORE_TOKEN=env(''),
 
     host=env('host'),
     dbname=env('dbname'),
