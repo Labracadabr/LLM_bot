eed8ed30239c81c772e1ed315d0a982585194804
Dmitrii <dminokin@gmail.com>
2024-05-25 21:52:33 +0300

    механика сообщений юзер-админ

--- handlers/handler_user_talk_to_admin.py
+++ handlers/handler_user_talk_to_admin.py
@@ -37,7 +37,9 @@ async def cancel(msg: Message, bot: Bot, state: FSMContext):
 
     language = get_user_info(user=user).get('lang')
     lexicon = load_lexicon(language)
+    await bot.delete_message(chat_id=user, message_id=msg.message_id)
     await bot.edit_message_text(chat_id=user, message_id=msg.message_id-1, text=lexicon['cancel'])
+    await state.clear()
 
 
 # сообщение админу
@@ -47,13 +49,16 @@ async def to_admin(msg: Message, bot: Bot, state: FSMContext):
     await log(logs, user, msg.text)
 
     # отправить админу
-    msg_to_admin = f'📩 Сообщение от {contact_user(msg.from_user)}:\n{msg.text}'
+    user_txt = f"<i>{msg.text}</i>"
+    msg_to_admin = f'📩 Сообщение от {contact_user(msg.from_user)}:\n{user_txt}'
     for ad in admins:
         await bot.send_message(text=msg_to_admin, chat_id=ad, parse_mode='HTML')
 
     # уведомить юзера
     language = get_user_info(user=user).get('lang')
     lexicon = load_lexicon(language)
-    await bot.edit_message_text(chat_id=user, message_id=msg.message_id-1, text=lexicon['admin_sent'])
+    await bot.delete_message(chat_id=user, message_id=msg.message_id)
+    await bot.edit_message_text(chat_id=user, message_id=msg.message_id-1,
+                                text=lexicon['admin_sent']+user_txt, parse_mode='HTML')
     await state.clear()
 
